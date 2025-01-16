c9a9dff2f8e330009862906f8ef8733138703759
Arnavjot <134613166+arnavjot@users.noreply.github.com>
2025-01-16 08:50:44 +0000

    conversion operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -37,5 +37,39 @@ let boolIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 56
 let stringNum = String(someNumber)
-console.log(typeof stringNum)
-console.log(stringNum);
+// console.log(typeof stringNum)
+// console.log(stringNum);
+
+
+//****************************** Operations ****************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2**3)
+// console.log(12%3)
+
+let str1 = "hello"
+let str2 = " anku"
+
+let str3 = str1 + str2 //concatenation 
+// console.log(str3);
+
+// console.log("1" + 2) // 12
+// console.log(1 + "2") // 12
+// console.log("1" + 2 + 2) // 122
+// console.log(1 + 2 + "2") // 32
+// console.log("1" + (2 + 2)) // 14
+
+// console.log(+true) // outputs 1
+// console.log(+"") // outputs 0
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2 //dont use this - ruins the readability
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter++);
+
