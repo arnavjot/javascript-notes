3f95143fe793fb1bdbab8079de1574b9a22aa7e6
Arnavjot <134613166+arnavjot@users.noreply.github.com>
2025-02-03 07:36:30 +0000

    summary

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -13,6 +13,11 @@ console.log(null > 0); // false
 console.log(null == 0); // false
 console.log(null >= 0); // true - happens due to conversion - comparisons >, <, >=, <= convert null to number treating it as a 0 whereas equality check == doesn't convert thats 
 
+
+console.log(undefined == 0); // false
+console.log(undefined > 0); // false
+console.log(undefined < 0); // false
+
 // === 
 
 console.log("2" === 2); // strict check
