c0861ec416deb34af6342c7acbc4501913cc3119
Arnavjot Kaur <arnavjotkaur.27@gmail.com>
2025-02-10 09:33:21 +0000

    created 02_arrays

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,33 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+// Push added array to array which is not what we wanted
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+const allHeros = marvel_heros.concat(dc_heros)
+console.log(allHeros); // output : [ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+const all_new_heros = [...marvel_heros, ...dc_heros] 
+console.log(all_new_heros); // output : [ 'thor', 'Ironman', 'spiderman', 'superman', 'flash', 'batman' ]
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+// const real_another_array = another_array.flat(1) //Returns a new array with all sub-array elements concatenated into it recursively up to the specified depth.
+// const real_another_array = another_array.flat(2) //upto level 2 or if many levels just say Infinity 
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+console.log(Array.isArray("Hitesh")) // false
+console.log(Array.from("Hitesh")) // converted it to an array
+console.log(Array.from({name: "hitesh"})) // output : [] 
+// {name: "hitesh"} can't convert this object to an array directly
+
+console.log(Array.of("Hitesh"))
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
