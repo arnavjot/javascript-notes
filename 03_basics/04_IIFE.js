924a0edf390d95226042921233cf954df51f51a6
Arnavjot Kaur <arnavjotkaur.27@gmail.com>
2025-02-10 15:13:41 +0000

    IIFE updation

--- 03_basics/04_IIFE.js
+++ 03_basics/04_IIFE.js
@@ -4,7 +4,12 @@
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
-})(); // Sometimes there is a problem with pollution in global scope - whatever declarations/ variables in global space are there to avoid that we use IIFE
+})();// Sometimes there is a problem with pollution in global scope - whatever declarations/ variables in global space are there to avoid that we use IIFE
+
+(function chai2(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})() // Semicolon not there so there will be an error - because IIFE doesn't know where to stp the context - to end add ;
 
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
