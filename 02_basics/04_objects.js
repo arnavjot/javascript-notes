a20c8f7309934118b9b404c83abd80d1aaeb643f
Arnavjot Kaur <arnavjotkaur.27@gmail.com>
2025-02-10 11:14:34 +0000

    updating 04_objects.js

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -23,11 +23,12 @@ const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
-// const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// const obj3 = { obj1, obj2 } //adds object to object
 
-const obj3 = {...obj1, ...obj2}
-// console.log(obj3);
+//const obj3 = Object.assign(obj1, obj2, obj4)
+//const obj3 = Object.assign({}, obj1, obj2, obj4) // good practice to add {}
+const obj3 = {...obj1, ...obj2,...obj4}
+console.log(obj3);
 
 
 const users = [
@@ -48,13 +49,14 @@ const users = [
 users[1].email
 // console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); // checks if property exists or not in the object
 
 
+//de-structuring
 const course = {
     coursename: "js in hindi",
     price: "999",
@@ -63,9 +65,9 @@ const course = {
 
 // course.courseInstructor
 
-const {courseInstructor: instructor} = course
-
+// const {courseInstructor} = course
 // console.log(courseInstructor);
+const {courseInstructor: instructor} = course
 console.log(instructor);
 
 // {
