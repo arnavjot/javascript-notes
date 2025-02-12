95d11ac4438185e905abddf3e5f83c39022b948d
Arnavjot Kaur <arnavjotkaur.27@gmail.com>
2025-02-12 10:05:02 +0000

    control flow created

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,47 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+let day = 3;
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+    case 2:
+        console.log("Tuesday");
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+    case 5:
+        console.log("Friday");
+    default:
+        console.log("Weekend");
+}
