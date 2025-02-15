3c9975891cb7b3411d591668c97dbb783f5d2d07
Arnavjot Kaur <arnavjotkaur.27@gmail.com>
2025-02-15 14:52:20 +0000

    updated 03_basics

--- 03_basics/03_This_and_arrow.js
+++ 03_basics/03_This_and_arrow.js
@@ -3,6 +3,7 @@ const user = {
     price: 999,
 
     welcomeMessage: function() {
+        console.log(`${user.username} , welcome to website`);
         console.log(`${this.username} , welcome to website`);
         console.log(this);
     }
